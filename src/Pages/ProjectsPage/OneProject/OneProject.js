4e20b1a72bfac4fe0a69933e076aeeba9fe9c0d3
hamzajorfaoui <hamzajorfaoui@gmail.com>
2020-07-16 21:57:26 +0100

    complete projects

---
+++ src/Pages/ProjectsPage/OneProject/OneProject.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './OneProject.scss';
+import Pic_project from'../../../Assets/P1.jpg';
+import gsap from 'gsap';
+
+export default function OneProject({close}){
+
+    React.useEffect(()=>{
+        if(close == false){
+            Close_Info();
+        }else if(close){
+            gsap.from(current_Picture.current,{duration:1,opacity:0,x:100});
+            gsap.from(current_Details.current,{duration:1,opacity:0,x:-100});
+        }
+    },[close]);
+    React.useEffect(()=>{
+        function start(){
+            // gsap.from(".Project",{duration:0.1,opacity:0,delay:1});
+            // gsap.from(".Project .oneProject .pic",{duration:1,opacity:0,x:100,delay:1});
+            // gsap.from(".Project .details",{duration:1,opacity:0,y:100,stagger:0,delay:1});
+            console.log(close)
+          }
+          start();  
+          return function clean(){
+          };
+    },[]);
+    const [Openinfo , setOpeninfo] =React.useState(false);
+    const Open_Info=()=>{
+        current_P.current.classList.add("open");
+     setOpeninfo(true);
+    }
+    const Close_Info=()=>{
+        current_P.current.classList.remove("open");
+        setOpeninfo(false);
+    }
+    var current_P= React.useRef(null);
+    var current_Picture= React.useRef(null);
+    var current_Details= React.useRef(null);
+    return(
+           <div className="oneProject" id="OneP" ref={current_P}>
+               <div className="pic" ref={ current_Picture}>
+                     <img src={Pic_project}></img>
+               </div>
+            <div className="details" ref={current_Details}>
+                <div className="title">
+                    <span>Project</span>
+                    <span>Project</span>
+                </div>
+
+                <div className="info" onClick={!Openinfo ?Open_Info:Close_Info}>
+                    <div>
+                        <span>{!Openinfo ?"Infos":"close"}</span>
+                        <span className="line"></span>
+                        <span className="circle"></span>
+                        <span className="icon-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 53">
+                                <path d="M3.13,35.325A25,25,0,0,0,51.53,26.5" fill="none" stroke="#fff" style={{strokeDashoffset: 0, strokeDasharray: null}}></path>
+                                <path d="M3.13,17.674A25,25,0,0,1,51.53,26.5" fill="none" stroke="#fff" style={{strokeDashoffset: 0, strokeDasharray: null}}></path>
+                            </svg>                    
+                        </span>                    
+                    </div>
+                </div>
+
+                <div className="infoComplete">
+                 <div className="_title"><h1>About</h1></div>
+                 <div className="About_text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate </div>
+                 <div className="_title"><h1>Technologie</h1></div>
+                 <div className="Technologies_text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,</div>
+                 <div className="_title"><h1>Project Source File</h1></div>
+                 <div className="ProjectSource_text">Lorem ipsum dolor sit amet, consectetur adipiscing </div>
+                </div>
+            </div>
+
+
+           </div>
+    )
+}
--- src/Pages/ProjectsPage/Project.js
+++ src/Pages/ProjectsPage/Project.js
@@ -1,71 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Project.scss';
-import Pic_project from'../../Assets/P1.jpg';
-import gsap from 'gsap/gsap-core';
+import gsap from 'gsap';
+import OneProject from './OneProject/OneProject';
 
 export function project_animation_leave(){
     gsap.to(".Project .oneProject .pic",{duration:1,opacity:0,x:100});
     gsap.to(".Project .details",{duration:1,opacity:0,y:100,stagger:0});
+    gsap.to('.line2',{duration:1 , y:0});
 }
+const Projects = [{close:false},{close:false},{close:false},{close:false},{close:false}];
+var selected = 0;
 export default function Project(){
 
+    const [my_Projects , SetmP] = useState(Projects);
+    var timeoutID = null;
+    const Projects_Scroll_add=()=>{
+          document.getElementById('Projects').addEventListener('wheel',Projects_Scroll_event);
+    }
+    const Projects_Scroll_remove=()=>{
+        document.getElementById('Projects').removeEventListener('wheel',Projects_Scroll_event);
+  }
+    const timeoutcalc=()=>{ 
+        Projects_Scroll_remove();
+        timeoutID = setTimeout(()=>{
+                    Projects_Scroll_add();
+                    timeoutID = null;  
+                    },1000);
+    }
+    const scroll_up=()=>{
+        timeoutcalc();
+        my_Projects[selected].close=false;
+        selected--;
+        my_Projects[selected].close=true;
+        SetmP([...my_Projects]);
+        gsap.to('#Projects',{y:-(selected*100)+"vh" , duration:1, onComplete:()=>{ }});
+    }
+    const scroll_down=()=>{
+        timeoutcalc();
+        my_Projects[selected].close=false;
+        selected++;
+        my_Projects[selected].close=true;
+        SetmP([...my_Projects]);
+        gsap.to('#Projects',{y:-(selected*100)+"vh" , duration:1 , onComplete:()=>{}});
+    }
+    const Projects_Scroll_event=(e)=>{
+            if(timeoutID == null){
+              if(e.deltaY > 0 && selected<Projects.length-1){
+                scroll_down();
+              }else if(e.deltaY < 0 && selected>0){
+                scroll_up();
+              }             
+            }
+    }
     React.useEffect(()=>{
         function start(){
-            gsap.from(".Project",{duration:0.1,opacity:0,delay:1});
-            gsap.from(".Project .oneProject .pic",{duration:1,opacity:0,x:100,delay:1});
-            gsap.from(".Project .details",{duration:1,opacity:0,y:100,stagger:0,delay:1});
+            gsap.from(".Project",{duration:0.5,opacity:0,delay:1,
+                                  onComplete:()=>{ 
+                                    my_Projects[selected].close=true;
+                                    SetmP([...my_Projects]);
+                                    gsap.to('#Projects',{y:-(selected*100)+"vh" , duration:1 , onComplete:()=>{}});
+                                    gsap.to('.P',{duration:0.5,opacity:1})
+                                  }});
+            gsap.from('numbers span',{duration:1 , opacity:0 , stagger:0.3});
+            gsap.to('.line2',{duration:1 , y:"-100px"});
+            Projects_Scroll_add();
+            
           }
           start();  
           return function clean(){
           };
     },[])
-    const [Openinfo , setOpeninfo] =React.useState(false);
-    const Open_Info=()=>{
-     document.getElementById('OneP').classList.add("open");
-     setOpeninfo(true);
-    }
-    const Close_Info=()=>{
-        document.getElementById('OneP').classList.remove("open");
-        setOpeninfo(false);
-    }
     return(
-        <div className="Project"> 
-           <div className="oneProject" id="OneP">
-               <div className="pic">
-                     <img src={Pic_project}></img>
-               </div>
-            <div className="details">
-                <div className="title">
-                    <span>Project</span>
-                    <span>Project</span>
-                </div>
-
-                <div className="info" onClick={!Openinfo ?Open_Info:Close_Info}>
-                    <div>
-                        <span>{!Openinfo ?"Infos":"close"}</span>
-                        <span className="line"></span>
-                        <span className="circle"></span>
-                        <span className="icon-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 53">
-                                <path d="M3.13,35.325A25,25,0,0,0,51.53,26.5" fill="none" stroke="#fff" style={{strokeDashoffset: 0, strokeDasharray: null}}></path>
-                                <path d="M3.13,17.674A25,25,0,0,1,51.53,26.5" fill="none" stroke="#fff" style={{strokeDashoffset: 0, strokeDasharray: null}}></path>
-                            </svg>                    
-                        </span>                    
-                    </div>
-                </div>
-
-                <div className="infoComplete">
-                 <div className="_title"><h1>About</h1></div>
-                 <div className="About_text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate </div>
-                 <div className="_title"><h1>Technologie</h1></div>
-                 <div className="Technologies_text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,</div>
-                 <div className="_title"><h1>Project Source File</h1></div>
-                 <div className="ProjectSource_text">Lorem ipsum dolor sit amet, consectetur adipiscing </div>
-                </div>
-            </div>
-
-
-           </div>
+        <>
+        <div className="Project" id="Projects"> 
+        {
+        my_Projects.map((proj, index)=>{
+        return<div className="P" key={index}>
+                <OneProject close={proj.close}></OneProject> 
+              </div>            
+        })
+        }
         </div>
+        <div className="numbers">
+        {
+        my_Projects.map((proj, index)=>{
+        return(<span className={proj.close?"active":""} key={index}>{"0"+(index+1)}</span>)            
+        })
+        }
+        </div>
+        </>
     )
 }
